5891a437a75e93caa9f69dfa8c6be6f572137155
Amit Masram <amitmasram831@gmail.com>
2023-12-17 19:38:13 +0530

    IBMI

--- finstagram_app/lib/main.dart
+++ finstagram_app/lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:finstagram_app/pages/home_page.dart';
 import 'package:finstagram_app/pages/login_page.dart';
 import 'package:finstagram_app/pages/register_page.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+void main() async {
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+);
   runApp(const MyApp());
 }
 
